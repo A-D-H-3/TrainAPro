37889638749131c3202ad91a4c1e1581a54c97a5
Avery Hampton <averydhampton@icloud.com>
2023-03-30 05:41:14 -0500

    adds collapsible styling to tap story list

--- TAP Story List/3 Client Script.js
+++ TAP Story List/3 Client Script.js
@@ -7,13 +7,37 @@ api.controller = function ($rootScope, $scope) {
   });
 
   function loadClass(id) {
-		console.log(id);
+    console.log(id);
     c.data.sys_id = id;
     c.data.action = "loadClass";
     c.server.update().then(function () {
       c.data.action = undefined;
     });
+  }	
+	
+/*	function decodeHTML(str) {
+    var a = str.replace(/<\/?[^>]+(>|$)/g, ""); //Remove tags
+    var b = a.replace(/&amp;/g, '&'); //Retain any ampersands that are just ampersands
+    return b.replace(/&#(\d+);/g, function(match, dec) {
+        return String.fromCharCode(dec); //Returns the special character from the decimal code representation and returns the entire decoded string.
+    });
+}
+	decodeHTML("test");
+	*/
+	
+/*	  $scope.changeSelected = function (id) {
+    for (var k = 0; k < c.data.classes.length; k++) {
+      c.data.classes[k].selected = false;
+    }
+
+    for (var j = 0; j < c.data.classes.length; j++) {
+      if (c.data.classes[j].sys_id == id) {
+        c.data.classes[j].selected = true;
+        //console.log(c.data.classes);
+        break;
+      }
+    }
+
   }
-	
-	
+	*/
 };
--- TAP Story List/4 Server Script.js
+++ TAP Story List/4 Server Script.js
@@ -1,25 +1,26 @@
 (function () {
-	/* populate the 'data' object */
-	/* e.g., data.table = $sp.getValue('table'); */
+  /* populate the 'data' object */
+  /* e.g., data.table = $sp.getValue('table'); */
 
-	data.storyList = [];
+  data.storyList = [];
 
-	if (input && input.action === "loadClass") {
-		storyList(input.sys_id);
-	}
+  if (input && input.action === "loadClass") {
+    storyList(input.sys_id);
+  }
 
-	function storyList(classID) {
-		var storyGR = new GlideRecordSecure("x_adsr_tap_story");
-		storyGR.addQuery("module", classID);
-		// storyGR.addQuery("x_adsr_tap_story.module", input.moduleID);
-		storyGR.query();
-		while (storyGR.next()) {
-			var storyObj = {
-				name: storyGR.name.getDisplayValue(),
-				acceptanceCri:
-				storyGR.acceptance_criteria.getDisplayValue(),
-			};
-			data.storyList.push(storyObj);
-		}
-	}
+  function storyList(classID) {
+    var storyGR = new GlideRecordSecure("x_adsr_tap_story");
+    storyGR.addQuery("module", classID);
+    // storyGR.addQuery("x_adsr_tap_story.module", input.moduleID);
+    storyGR.query();
+    while (storyGR.next()) {
+      var storyObj = {
+        name: storyGR.name.getDisplayValue(),
+        criteria: storyGR.acceptance_criteria.getDisplayValue(),
+				sys_id: storyGR.getDisplayValue("sys_id"),
+				desc: storyGR.getDisplayValue("description")
+      };
+      data.storyList.push(storyObj);
+    }
+  }
 })();
